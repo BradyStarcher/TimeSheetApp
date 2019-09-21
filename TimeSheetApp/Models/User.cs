7d8654af51d4ed207b57a598f5805f4796202fe5
Samantha Burkey <sburkey@wvup.edu>
2019-09-21 13:27:55 -0400

    Created user model. Still needs refining

---
+++ TimeSheetApp/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheetApp.Models
+{
+    [Table("Users", Schema = "TS")]
+    public class User : IdentityUser
+    {
+        public User() : base()
+        {
+
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; } 
+    }
+}
