5bab42adcd090c0ee125579fc41362a9ec9d1996
Samantha Burkey <sburkey@wvup.edu>
2019-10-06 17:49:17 -0400

    Created view models

---
+++ TimeSheetApp/Models/EmployeeAndManagerListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheetApp.Models
+{
+    public class EmployeeAndManagerListViewModel
+    {
+        public Employee[] Employees { get; set; }
+
+        public Manager[] Managers { get; set; }
+    }
+}
