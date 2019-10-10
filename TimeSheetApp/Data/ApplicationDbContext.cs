085c7383994ceb7be7b6f421b634833caf7879f7
Brady <bradystarcher@hotmail.com>
2019-10-10 11:58:01 -0400

    Added role to models

--- TimeSheetApp/Data/ApplicationDbContext.cs
+++ TimeSheetApp/Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TimeSheetApp.Models;
 
 namespace TimeSheetApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User, Role, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ TimeSheetApp/Models/Role.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheetApp.Models
+{
+    public class Role : IdentityRole
+    {
+        public Role() : base()
+        {
+
+        }
+
+        public Role(string roleName) : base(roleName)
+        {
+
+        }
+
+        public Role(string roleName, string description) : base(roleName)
+        {
+            this.Description = description;
+        }
+
+        public string Description { get; set; }
+    }
+}
